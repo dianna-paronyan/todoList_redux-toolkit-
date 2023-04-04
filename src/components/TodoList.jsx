d861eee2b9b034908ec7fe598af82e7c9197c1e8
dianna1234 <01234dianna@gmail.com>
2023-04-04 15:04:31 +0400

    add edit todo functionality

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,21 +1,47 @@
 import { useDispatch, useSelector } from "react-redux"
-import { deleteTodo } from "../features/todos-slice";
+import { deleteTodo, editTodo } from "../features/todos-slice";
+import { useState } from "react";
 import './TodoList.css'
 
 
 function TodoList() {
+    const [input,setInput] = useState('');
     const todos = useSelector((state)=>state.todos);
     const dispatch = useDispatch();
     function delTodo(id){
         dispatch(deleteTodo({id}))
     }
+    const handleTodoItemClick =(id,text) =>(e)=>{
+        dispatch(editTodo({id, text, isEditMode: true}))
+        console.log(todos);
+    }
+    const todoItemFormSubmit = (id) =>(e)=>{
+        e.preventDefault();
+        dispatch(editTodo({ id, text: input, isEditMode: false }))
+    }
+    const  handleTodoItemChange = (id)=> (e)=>{
+        setInput(e.target.value);
+        dispatch(editTodo({id, text: e.target.value, isEditMode:true}))
+        console.log(todos);
+    }
+
+
   return (
     <div className="list_box">
         {todos.map((el)=>{
             return(
                 <div key={el.id} className="item_box">
-                    <p>{el.text}</p>
-                    <button className="delete_btn" onClick={()=>delTodo(el.id)}>x</button>
+                    {!el.isEditMode ? (
+                        <>
+                            <p  onClick={handleTodoItemClick(el.id,el.text)}>{el.text}</p>
+                            <button className="delete_btn" onClick={()=>delTodo(el.id)}>x</button>
+                        </>
+                    ):(
+                        <form action="" className="form_content" onSubmit={todoItemFormSubmit(el.id)} >
+                            <input type="text" className='input_field' value={el.text}  onChange={handleTodoItemChange(el.id)}  />
+                            <button className='add_btn'>done</button>
+                        </form>
+                    )}
                 </div>
             )
         })}
--- src/features/todos-slice.js
+++ src/features/todos-slice.js
@@ -3,21 +3,33 @@ import { createSlice, nanoid } from "@reduxjs/toolkit";
 const initialState = [];
 
 const todosSlice = createSlice({
-    name:'todos',
-    initialState,
-    reducers: {
-        addTodo:(state,action)=>{
-            const newTodo = {
-                id: nanoid(),
-                text:action.payload.text,
+  name: "todos",
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      const newTodo = {
+        id: nanoid(),
+        text: action.payload.text,
+        isEditMode: false,
+      };
+      state.push(newTodo);
+    },
+    deleteTodo: (state, action) => {
+      return state.filter((todo) => todo.id !== action.payload.id);
+    },
+    editTodo: (state, action) => {
+      return state.map((item) =>
+        item.id === action.payload.id
+          ? {
+              ...item,
+              text: action.payload.text,
+              isEditMode: action.payload.isEditMode,
             }
-            state.push(newTodo);
-        },
-        deleteTodo:(state,action)=>{
-          return  state.filter((todo)=> todo.id !== action.payload.id)
-        }
-    }
-})
+          : item
+      );
+    },
+  },
+});
 
-export const {addTodo,deleteTodo} = todosSlice.actions;
+export const { addTodo, deleteTodo, editTodo } = todosSlice.actions;
 export default todosSlice.reducer;
